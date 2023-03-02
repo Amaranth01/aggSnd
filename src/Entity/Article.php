30e220da65c88159eb87ac8d4114e40915dc90fb
Amaranth01 <vanessa.nectoux@gmail.com>
2023-03-02 11:02:00 +0100

    Making Entity and add webpack

---
+++ src/Entity/Article.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    private ?User $user_id = null;
+
+    #[ORM\OneToMany(mappedBy: 'article', targetEntity: Comment::class, orphanRemoval: true)]
+    private Collection $comment_id;
+
+    public function __construct()
+    {
+        $this->comment_id = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getCommentId(): Collection
+    {
+        return $this->comment_id;
+    }
+
+    public function addCommentId(Comment $commentId): self
+    {
+        if (!$this->comment_id->contains($commentId)) {
+            $this->comment_id->add($commentId);
+            $commentId->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommentId(Comment $commentId): self
+    {
+        if ($this->comment_id->removeElement($commentId)) {
+            // set the owning side to null (unless already changed)
+            if ($commentId->getArticle() === $this) {
+                $commentId->setArticle(null);
+            }
+        }
+
+        return $this;
+    }
+}
